a6672877f8d40c2990d89a3b181bcce3b140e6f9
aarberkowitz <aarberkowitz@gmail.com>
2025-11-24 20:41:32 +0200

    C: Completed Bookbot!

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from stats import *
+import sys
+
+
+
 
 def main():
-	frankensteins_text = get_book_text("books/frankenstein.txt")
-	word_counter(frankensteins_text)
+
+	frankensteins_text = get_book_text(book_path)
 	char_dict = character_counter(frankensteins_text)
-	print(char_dict)
+	sorted = sorter(char_dict)
+	print('=' * 12 + " BOOKBOT " + '=' * 12)
+	print(f"Analyzing book found at {book_path}...")
+	print('-' * 12 + " Word Count " + '-' * 12)
+	print(word_counter(frankensteins_text))
+	print('-' * 12 + " Character Count " + '-' * 12)
+	for item in sorted:
+		print(f"{item['char']}: {item['num']}")
+	print('=' * 15 + " END " + '=' * 15)
+
 
 def get_book_text(filepath):
 	with open(filepath) as f:
 		return f.read()
 
+def usage_check():
+	if len(sys.argv) != 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	return False
+
+usage_check()
+book_path = sys.argv[1]
 main()
--- stats.py
+++ stats.py
@@ -1,14 +1,28 @@
 def word_counter(text_of_book):
 	num_words = len(text_of_book.split())
-	return print(f"Found {num_words} total words")
+	return f"Found {num_words} total words"
 
 def character_counter(text):
-	characters = {}
+	counts = {}
+	characters = []
 
 	text = text.lower()
 	for letter in text:
-		if letter not in characters:
-			characters[letter] = 1
+		if letter not in counts:
+			counts[letter] = 1
 		else:
-			characters[letter] += 1
+			counts[letter] += 1
+	for letter, num in counts.items():
+		if letter.isalpha():
+			characters.append({
+				"char": letter,
+				"num": num
+				})
 	return characters
+
+def sort_on(items):
+	return items["num"]
+
+def sorter(items):
+	items.sort(reverse=True, key=sort_on)
+	return items
